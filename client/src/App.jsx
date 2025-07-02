1df190e6c8c784649dcb99d9270e049c713f0e6c
Annonnymmousss <jatin02012006@gmail.com>
2025-07-02 08:35:48 +0530

    fix a bug

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import LoginPage from './pages/LoginPage'
+import ProfilePage from './pages/ProfilePage'
 
 const App = () => {
   return (
