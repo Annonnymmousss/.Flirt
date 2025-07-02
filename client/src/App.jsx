5dcd1be25181475f6c730666c31478cdd2ae4a30
Annonnymmousss <jatin02012006@gmail.com>
2025-07-02 10:08:28 +0530

    feat(frontend):added Homepage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,13 +4,14 @@ import HomePage from './pages/HomePage'
 import LoginPage from './pages/LoginPage'
 import ProfilePage from './pages/ProfilePage'
 
+
 const App = () => {
   return (
-    <div>
+    <div className="bg-[url('./src/assets/bgImage.svg')] bg-contain" >
       <Routes>
-        <Route path='/' element={HomePage}/>
-        <Route path='/login' element={LoginPage}/>
-        <Route path='/profile' element={ProfilePage}/>
+        <Route path='/' element={<HomePage/>}/>
+        <Route path='/login' element={<LoginPage/>}/>
+        <Route path='/profile' element={<ProfilePage/>}/>
       </Routes>
     </div>
   )
