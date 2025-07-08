79a23f8b0d8a6d5f82a9880efb44e8b184de57ca
Annonnymmousss <jatin02012006@gmail.com>
2025-07-08 07:04:09 +0530

    feat(backend):added signUp logic

---
+++ server/controllers/userController.js
@@ -0,0 +1,32 @@
+import e from "express"
+import { generateToken } from "../lib/utils.js"
+import User from "../models/User.js"
+import bcrypt from 'bcryptjs'
+
+const signUp = async(req,res) => {
+    const {FullName , email , password , bio} = req.body
+    try {
+        if (!FullName || !email || !password || !bio) {
+            return res.json({ success: false, message: "Missing details" })
+        }
+        const user = await User.findOne({ email })
+        if (user) {
+            return res.json({ success: false, message: "user already exist" })
+        }
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password,salt)
+        const newUser = await User.create({
+            FullName , email , password:hashedPassword , bio
+        })
+        const token = generateToken(newUser._id)
+        res.json({success:true , userData:newUser , token , message:"Account created successfully"
+        })
+    } catch (error) {
+        console.log(error)
+        res.json({success:false , message:error.message})
+    }
+}
+
+export default {
+    signUp
+}
