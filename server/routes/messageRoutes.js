ac52fe736257915bb64ba26598ddaf1f81233a94
Annonnymmousss <jatin02012006@gmail.com>
2025-07-09 07:48:22 +0530

    feat(backend):added message algos and setup routes

---
+++ server/routes/messageRoutes.js
@@ -0,0 +1,8 @@
+import express from 'express'
+import { protectRoute } from '../middleware/auth.js'
+import { getMessages, getUsersForSidebar, markMessageSeen } from '../controllers/messageController.js'
+const messageRouter = express.Router()
+messageRouter.get('/user' , protectRoute , getUsersForSidebar)
+messageRouter.get('/:id' , protectRoute , getMessages)
+messageRouter.put('mark/:id' , protectRoute , markMessageSeen)
+export default messageRouter
